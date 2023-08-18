eb5b93889fedf6f9004f075b4d48c63b7ec65c8a
Agustin Schulze <agsp3rs3o@gmail.com>
2023-08-18 15:06:21 -0300

    first commit

---
+++ springboot-servicio-productos/src/main/java/com/pringboot/app/products/model/dao/ProductDao.java
@@ -0,0 +1,13 @@
+package com.pringboot.app.products.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.pringboot.app.products.model.entity.Product;
+
+public interface ProductDao extends CrudRepository<Product, Long>{
+	
+	
+	
+	
+
+}
