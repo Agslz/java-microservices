106ff9813de1ec1eb485ed50a75815017bab001c
Agustín Schulze <agsp3rs3o@gmail.com>
2024-02-07 11:18:15 -0300

    Big Update - Chapter 66

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,13 +1,20 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.Map;
+//import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -18,16 +25,23 @@ import com.formacionbdi.springboot.app.item.models.Item;
 import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+//import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+//import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
 	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
+
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -47,7 +61,7 @@ public class ItemController {
 				e -> metodoAlternativo(id, cantidad, e));
 	}
 
-	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+	/*@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
 	public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
@@ -58,7 +72,7 @@ public class ItemController {
 	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
 	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
-	}
+	}*/
 
 	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
 		logger.info(e.getMessage());
@@ -73,8 +87,8 @@ public class ItemController {
 		return item;
 	}
 
-	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
-		
+	/*public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+
 		logger.info(e.getMessage());
 		Item item = new Item();
 		Producto producto = new Producto();
@@ -85,6 +99,21 @@ public class ItemController {
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 		return CompletableFuture.supplyAsync(() -> item);
+	}*/
+
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		logger.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("desarrollo")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 }
