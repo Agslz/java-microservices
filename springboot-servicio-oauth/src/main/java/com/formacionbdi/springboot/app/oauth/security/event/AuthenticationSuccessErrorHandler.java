51d8ff6e86420044411044a9b3e0f6c8af387d16
Agustín Schulze <agsp3rs3o@gmail.com>
2024-03-08 12:09:55 -0300

    Big Update - Chapter 109

---
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
@@ -0,0 +1,77 @@
+package com.formacionbdi.springboot.app.oauth.security.event;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
+import com.formacionbdi.springboot.app.oauth.services.IUsuarioService;
+
+import feign.FeignException;
+
+@Component
+public class AuthenticationSuccessErrorHandler implements AuthenticationEventPublisher {
+
+	private Logger log = LoggerFactory.getLogger(AuthenticationSuccessErrorHandler.class);
+
+	@Autowired
+	private IUsuarioService usuarioService;
+
+	@Override
+	public void publishAuthenticationSuccess(Authentication authentication) {
+
+		// if(authentication.getName().equalsIgnoreCase("frontendapp")){
+		if (authentication.getDetails() instanceof WebAuthenticationDetails) {
+			return;
+		}
+
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		String messageString = "Sucess Login: " + userDetails.getUsername();
+		System.out.println(messageString);
+		log.info(messageString);
+
+		Usuario usuario = usuarioService.findByUsername(authentication.getName());
+
+		if (usuario.getIntentos() != null && usuario.getIntentos() > 0) {
+			usuario.setIntentos(0);
+			usuarioService.update(usuario, usuario.getId());
+		}
+
+	}
+
+	@Override
+	public void publishAuthenticationFailure(AuthenticationException exception, Authentication authentication) {
+		String messaString = "Error en el Login: " + exception.getMessage();
+		log.error(messaString);
+		System.out.println(messaString);
+
+		try {
+			Usuario usuario = usuarioService.findByUsername(authentication.getName());
+			if (usuario.getIntentos() == null) {
+				usuario.setIntentos(0);
+			}
+
+			log.info("Intentos actual es de: " + usuario.getIntentos());
+
+			usuario.setIntentos(usuario.getIntentos() + 1);
+
+			if (usuario.getIntentos() >= 3) {
+				log.error("El usuario %s deshabilitado por maximo de intentos", usuario.getUsername());
+				usuario.setEnabled(false);
+			}
+
+			usuarioService.update(usuario, usuario.getId());
+
+		} catch (FeignException e) {
+			log.error(String.format("El usuario %s no existe en el sistema", authentication.getName()));
+		}
+
+	}
+
+}
